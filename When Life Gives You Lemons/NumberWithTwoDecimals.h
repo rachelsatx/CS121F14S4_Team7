44c190c1944922244960033003ec9ac7ca480b7d
Guest User <Guest@arden.cs.hmc.edu>
2014-11-17 17:30:04 -0800

    Implemented NumberWithTwoDecimals

---
+++ When Life Gives You Lemons/NumberWithTwoDecimals.h
@@ -0,0 +1,30 @@
+//
+//  NumberWithTwoDecimals.h
+//  When Life Gives You Lemons
+//
+//  Created by Guest User on 11/17/14.
+//  Copyright (c) 2014 Jonathan Finnell, Amit Maor, Joshua Petrack, Megan Shao, Rachel Wilson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NumberWithTwoDecimals : NSObject
+
+- (id) initWithIntegerPart:(int)integerPart andFractionalPart:(char)fractionalPart;
+- (id) initWithFloat:(float)number;
+
+- (int) integerPart;
+- (int) fractionalPart;
+
+- (BOOL) isEqual:(NumberWithTwoDecimals*)other;
+- (BOOL) isNotEqual:(NumberWithTwoDecimals*)other;
+- (BOOL) isGreaterThan:(NumberWithTwoDecimals*)other;
+- (BOOL) isGreaterThanOrEqual:(NumberWithTwoDecimals*)other;
+- (BOOL) isLessThan:(NumberWithTwoDecimals*)other;
+- (BOOL) isLessThanOrEqual:(NumberWithTwoDecimals*)other;
+
+- (NumberWithTwoDecimals*) add:(NumberWithTwoDecimals*)other;
+- (NumberWithTwoDecimals*) subtract:(NumberWithTwoDecimals*)other;
+- (NumberWithTwoDecimals*) scale:(NumberWithTwoDecimals*)other;
+
+@end
