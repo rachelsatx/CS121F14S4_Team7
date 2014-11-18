97a4f659d3b5c24bf35afc1a6751e7fbd6712dfd
Megan Shao <MeganShao@wl-208-84.wpa.claremont.edu>
2014-11-17 16:54:28 -0800

    Cleaned up code in view controllers.

--- When Life Gives You Lemons/PostDayViewController.m
+++ When Life Gives You Lemons/PostDayViewController.m
@@ -37,23 +37,23 @@
     _dataStore = dataStore;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    // Make sure your segue name in storyboard is the same as this line
+    // Make sure segue name in storyboard is the same as this line
     if ([[segue identifier] isEqualToString:@"PostDayToPreDay"])
     {
         // Get reference to the destination view controller
         PreDayViewController* preDayViewController = [segue destinationViewController];
         
-        // Pass any objects to the view controller here, like...
+        // Pass dataStore to the view controller
         [preDayViewController setDataStore:_dataStore];
     }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end
