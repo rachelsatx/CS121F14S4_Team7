93717827efd827df9764c6289c3e7aed083fba3f
Josh <jpetrack@hmc.edu>
2014-12-08 21:40:32 -0800

    Changed previous commit to 3 for gold.

--- When Life Gives You Lemons/Model.m
+++ When Life Gives You Lemons/Model.m
@@ -187,7 +187,7 @@
     
     // Check for perfection achievement.
     if ([self isPerfectBadges:badges]) {
-        [badges setValue:@4 forKey:allBadges];
+        [badges setValue:@3 forKey:allBadges];
     }
     
     
